ef33aac956182fec58e72c42d30b662b5f8f8648
Jakub Kramarz <jakub@hackerspace-krk.pl>
2018-07-14 20:57:15 +0200

    added transition frames

--- clock.py
+++ clock.py
@@ -4,8 +4,8 @@ import numpy as np
 import datetime
 from time import sleep
 
-def digitMatrix(x):
-    return {
+def digitMatrix(x, prev=None):
+    matrices = {
         0 : np.matrix('1 1 1; 1 0 1; 1 0 1; 1 0 1; 1 1 1'),
         1 : np.matrix('0 1 0; 1 1 0; 0 1 0; 0 1 0; 1 1 1'),
         2 : np.matrix('1 1 1; 0 0 1; 1 1 1; 1 0 0; 1 1 1'),
@@ -16,31 +16,39 @@ def digitMatrix(x):
         7 : np.matrix('1 1 1; 0 0 1; 0 1 0; 0 1 0; 1 0 0'),
         8 : np.matrix('1 1 1; 1 0 1; 1 1 1; 1 0 1; 1 1 1'),
         9 : np.matrix('1 1 1; 1 0 1; 1 1 1; 0 0 1; 1 1 1'),
-        10 : np.matrix('0 1 0; 1 0 0; 0 1 1; 1 0 0; 1 1 1'), #transition 0 -> 1
-        12 : np.matrix('0 1 0; 1 0 1; 0 1 0; 1 0 0; 1 1 1'),
-        23 : np.matrix('1 1 1; 0 0 1; 1 1 1; 0 1 0; 1 1 1'),
-        34 : np.matrix('1 0 1; 0 1 1; 1 1 1; 0 0 1; 0 1 1'),
-        45 : np.matrix('1 0 1; 1 1 0; 1 1 1; 0 0 1; 0 1 1'),
-        56 : np.matrix('1 1 1; 1 0 0; 1 1 1; 0 1 1; 1 1 1'),
-        67 : np.matrix('1 1 1; 0 1 0; 0 1 0; 0 1 0; 1 1 0'),
-        78 : np.matrix('1 1 1; 0 1 1; 0 1 0; 0 1 0; 1 1 0'),
-        89 : np.matrix('1 1 1; 1 0 1; 1 1 1; 0 1 1; 1 1 1'),
-        90 : np.matrix('1 1 1; 1 0 1; 1 0 1; 0 1 1; 1 1 1'),
-        20 : np.matrix('1 1 1; 0 1 1; 1 1 1; 1 1 0; 1 1 1'),
-        420 : np.matrix('0 0 0 ; 0 0 0; 0 0 0; 0 0 0; 0 0 0'),
-    }.get(x, np.matrix('0 0 0 ; 0 0 0; 0 0 0; 0 0 0; 0 0 0'))
+        (0, 1) : np.matrix('0 1 0; 1 0 0; 0 1 1; 1 0 0; 1 1 1'),
+        (1, 2) : np.matrix('0 1 0; 1 0 1; 0 1 0; 1 0 0; 1 1 1'),
+        (2, 3) : np.matrix('1 1 1; 0 0 1; 1 1 1; 0 1 0; 1 1 1'),
+        (3, 4) : np.matrix('1 0 1; 0 1 1; 1 1 1; 0 0 1; 0 1 1'),
+        (4, 5) : np.matrix('1 0 1; 1 1 0; 1 1 1; 0 0 1; 0 1 1'),
+        (5, 6) : np.matrix('1 1 1; 1 0 0; 1 1 1; 0 1 1; 1 1 1'),
+        (6, 7) : np.matrix('1 1 1; 0 1 0; 0 1 0; 0 1 0; 1 1 0'),
+        (7, 8) : np.matrix('1 1 1; 0 1 1; 0 1 0; 0 1 0; 1 1 0'),
+        (8, 9) : np.matrix('1 1 1; 1 0 1; 1 1 1; 0 1 1; 1 1 1'),
+        (9, 0) : np.matrix('1 1 1; 1 0 1; 1 0 1; 0 1 1; 1 1 1'),
+        (2, 0) : np.matrix('1 1 1; 0 1 1; 1 1 1; 1 1 0; 1 1 1'),
+        None : np.matrix('0 0 0 ; 0 0 0; 0 0 0; 0 0 0; 0 0 0'),
+    }
 
-def frame_number(x):
+    if prev is not None and x != prev:
+        return matrices.get( (prev,x), matrices[None] )
+    else:
+        return matrices.get(x, matrices[None])
+
+def frame_number(x, prev):
     as_string = str(x)
+    prev_string = str(prev)
     return np.concatenate(
         (
             [[0]]*5,
             digitMatrix(
-                0 if len(as_string) == 1 else int(as_string[0])
+                None if len(as_string) == 1 else int(as_string[0]),
+                None if len(prev_string) == 1 else int(prev_string[0]),
             ),
             [[0]]*5,
             digitMatrix(
-                int(as_string[0]) if len(as_string) == 1 else int(as_string[1])
+                int(as_string[0]) if len(as_string) == 1 else int(as_string[1]),
+                int(prev_string[0]) if len(prev_string) == 1 else int(prev_string[1]),
             ),
             [[0]]*5,
         ),
@@ -86,14 +94,20 @@ def empty_frame():
 client = mqtt.Client()
 client.connect("localhost", 1883, 60)
 
+prev = datetime.datetime.now()
 while True:
     now = datetime.datetime.now()
     client.publish(
         "display/_all/frame",
         frames_to_data(
-            frame_number(now.hour),
-            frame_number(now.minute),
-            frame_number(now.second)
+            frame_number(now.hour, prev.hour),
+            frame_number(now.minute, prev.minute),
+            frame_number(now.second, prev.second)
         )
     )
-    sleep(1)
+    delta = now - prev
+    if prev.hour == now.hour and prev.minute == now.minute and prev.second == now.second:
+        sleep(max(0.01, 0.99 - (delta.microseconds/100000)))
+    else:
+        sleep(0.01)
+    prev = now
